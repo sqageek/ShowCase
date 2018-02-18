6fa18b90d8ba1410aa8a8c05045a50ede6a5dd0f
sqageek <sa.testinggeek@gmail.com>
2018-02-18 15:17:04 +0530

    Modified templates to display provider information from database

--- getvid/views.py
+++ getvid/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Provider
+from .models import Course
 
 # Create your views here.
 
 def index(request):
     #return HttpResponse("Show statistics by Provider, Category.")
-    return render(request, "getvid/index.html",{})
-    
+    providersData = Provider.objects.values()
+    return render(request, 'getvid/index.html', {'providersData': providersData})
+    #return render(request, "getvid/index.html",{})
+
 def udemy(request):
     return HttpResponse("Show all UDEMY pending & completed courses.")
-    
+
 def lynda(request):
     return HttpResponse("Show all LYNDA pending & completed courses.")
